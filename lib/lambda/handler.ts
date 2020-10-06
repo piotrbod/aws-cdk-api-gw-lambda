457e86212dbff62e88d6ad6faf30607fbbe2c0ae
root <root@ip-192-168-0-54.eu-west-1.compute.internal>
2020-10-06 07:34:47 +0000

    Add Testing Account

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
-    body: 'Hello from a Lambda Function',
+    body: 'Hello from a Lambda Function - Morning 6th Oct 2020!',
     statusCode: 200,
   };
 }
